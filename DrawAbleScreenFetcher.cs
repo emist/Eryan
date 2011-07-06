de5126daaae87a9a3babc6455c9b4cbef4b08211
Igor Hernandez <emistz@gmail.com>
2011-07-06 06:14:12 -0400

    Singleton to holding all the drawable screens

---
+++ DrawAbleScreenFetcher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eryan
+{
+    public static class DrawAbleScreenFetcher
+    {
+        private static List<Utils> screens = new List<Utils>();
+
+        public static Utils fetch(uint pid)
+        {
+            foreach (Utils util in screens)
+            {
+                if (util.getPid() == pid)
+                    return util;
+            }
+            return null;
+        }
+
+        public static void addScreen(Utils screen)
+        {
+            lock (screens)
+            {
+                screens.Add(screen);
+            }
+        }
+    }
+}
