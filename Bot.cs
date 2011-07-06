81daf873a558ef6443504486552e4dbdafdc38b8
Igor Hernandez <emistz@gmail.com>
2011-07-06 06:16:00 -0400

    testing the new mouse input

--- Bot.cs
+++ Bot.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using System.Threading;
+using System.Drawing;
 
 
 namespace Eryan
@@ -30,8 +31,10 @@ namespace Eryan
             {
                 WindowHandler bot1 = client.getBotByOrder(0);
                 bot1.getKeyBoard().sendKeyPresses("HelloWorld", 100, 600);
-                bot1.getMouse();
-
+                bot1.getMouse().moveMouse(new Point(300, 300));
+                bot1.getMouse().move(new Point(50, 60));
+                Console.WriteLine("Bot1 pid: " + bot1.getPid());
+                
                 
 
                 System.Threading.Thread.Sleep(1000);
