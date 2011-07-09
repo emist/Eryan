5e32ebea07b522e9902e48170cccd8c60dc18c4d
emistz <emistz@gmail.com>
2011-07-09 15:05:36 -0400

    Implementing an observer pattern for events

---
+++ EryanEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eryan
+{
+    public class EryanEventHandler:BaseSubject
+    {
+        Event e;
+        public void dispatch(Event e)
+        {
+            this.e = e;
+            this.notifyObservers();
+        }
+
+    }
+}
