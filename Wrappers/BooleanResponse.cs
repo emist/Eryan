8ef78cdb57f55b59fa8b50e50697d389922ea3a1
emistz <emistz@gmail.com>
2011-07-12 04:27:26 -0400

    Wrapper object for boolean responses

---
+++ Wrappers/BooleanResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eryan.Wrappers
+{
+    public class BooleanResponse : Response
+    {
+        bool data = false;
+        byte[] input;
+        public BooleanResponse(byte[] input)
+        {
+            this.input = input;
+        }
+        public override void HandleResponse()
+        {
+            string output = Encoding.ASCII.GetString(input, 0, Array.IndexOf(input, (byte)0));
+            output =output.Trim();
+            //Console.WriteLine("HandleResponse: " + output);
+            data = output.Equals("True");
+        }
+        public override Object Data
+        {
+            get
+            {
+                return data;
+            }
+        }
+
+    }
+}
---
+++ Wrappers/Response.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eryan.Wrappers
+{
+    public abstract class Response
+    {
+        Object data;
+
+        public virtual void HandleResponse()
+        { }
+
+        public virtual Object Data
+        {
+            get
+            {
+                return data;
+            }
+        }
+    }
+
+}
