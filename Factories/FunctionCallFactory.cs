6fc50f0a51b5b4f230d111c5d073d6853f26c349
emistz <emistz@gmail.com>
2011-07-14 12:29:09 -0400

    Added response type field to implement a ResponseFactory

--- Factories/FunctionCallFactory.cs
+++ Factories/FunctionCallFactory.cs
@@ -5,41 +5,68 @@ using System.Text;
 
 namespace Eryan.Factories
 {
+    /// <summary>
+    /// This class builds functionCall objects
+    /// </summary>
     public class FunctionCallFactory
     {
         eveobjects.functionCall function;
 
         //Function calls this builder builds
 
-        public const string ATLOGIN = "atLogin";
-        public const string FINDBYNAME = "findByName";
+        public struct calls
+        {
+            public const string ATLOGIN = "atLogin";
+            public const string FINDBYNAME = "findByName";
+        }
 
-       
-        public eveobjects.functionCall build(string function)
+       /// <summary>
+       /// Builds a functionCall object with no parameters
+       /// </summary>
+       /// <param name="function">The function name to be called</param>
+       /// <param name="responsetype">The type of the expected response as given by the response struct in Responses</param>
+       /// <returns>The serializeable functionCall object</returns>
+        public eveobjects.functionCall build(string function, string responsetype)
         {
             this.function = new eveobjects.functionCall();
             eveobjects.functionCall.Builder builder = this.function.ToBuilder();
             builder.Name = function;
+            builder.Responsetype = responsetype;
             return builder.Build();
+
         }
 
-
-        public eveobjects.functionCall build(string function, string arg)
+        /// <summary>
+        /// Builds a functionCall object with one parameter
+        /// </summary>
+        /// <param name="function">The function name to be called</param>
+        /// <param name="arg">The parameter to pass with the functionCall object</param>
+        /// <param name="responsetype">The type of the expected response as given by the response struct in Responses</param>
+        /// <returns>The serializeable functionCall object</returns>
+        public eveobjects.functionCall build(string function, string arg, string responsetype)
         {
             this.function = new eveobjects.functionCall();
             eveobjects.functionCall.Builder builder = this.function.ToBuilder();
             builder.Name = function;
             builder.Strparameter = arg;
+            builder.Responsetype = responsetype;
             return builder.Build();
         }
 
-
-        public eveobjects.functionCall build(string function, List<string> arguments)
+        /// <summary>
+        /// Builds a functionCall object with a variable argument list
+        /// </summary>
+        /// <param name="function">The function name to be called</param>
+        /// <param name="arguments">A List of Strings containing the parameters to pass with the functionCall object</param>
+        /// <param name="responsetype">The type of the expected response as given by he response struct in Responses</param>
+        /// <returns>The serializeable functionCall object</returns>
+        public eveobjects.functionCall build(string function, List<string> arguments, string responsetype)
         {
 
             this.function = new eveobjects.functionCall();
             eveobjects.functionCall.Builder builder = this.function.ToBuilder();
             builder.Name = function;
+            builder.Responsetype = responsetype;
 
             for (int i = 0; i < arguments.Count; i++)
             {
