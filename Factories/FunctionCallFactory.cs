7523dca067f34d7c226095f9ec32e149b8846d2c
emistz <emistz@gmail.com>
2011-07-14 13:39:34 -0400

    Factories code cleanup

--- Factories/FunctionCallFactory.cs
+++ Factories/FunctionCallFactory.cs
@@ -14,7 +14,7 @@ namespace Eryan.Factories
 
         //Function calls this builder builds
 
-        public struct calls
+        public struct CALLS
         {
             public const string ATLOGIN = "atLogin";
             public const string FINDBYNAME = "findByName";
@@ -24,14 +24,12 @@ namespace Eryan.Factories
        /// Builds a functionCall object with no parameters
        /// </summary>
        /// <param name="function">The function name to be called</param>
-       /// <param name="responsetype">The type of the expected response as given by the response struct in Responses</param>
        /// <returns>The serializeable functionCall object</returns>
-        public eveobjects.functionCall build(string function, string responsetype)
+        public eveobjects.functionCall build(string function)
         {
             this.function = new eveobjects.functionCall();
             eveobjects.functionCall.Builder builder = this.function.ToBuilder();
             builder.Name = function;
-            builder.Responsetype = responsetype;
             return builder.Build();
 
         }
@@ -41,15 +39,13 @@ namespace Eryan.Factories
         /// </summary>
         /// <param name="function">The function name to be called</param>
         /// <param name="arg">The parameter to pass with the functionCall object</param>
-        /// <param name="responsetype">The type of the expected response as given by the response struct in Responses</param>
         /// <returns>The serializeable functionCall object</returns>
-        public eveobjects.functionCall build(string function, string arg, string responsetype)
+        public eveobjects.functionCall build(string function, string arg)
         {
             this.function = new eveobjects.functionCall();
             eveobjects.functionCall.Builder builder = this.function.ToBuilder();
             builder.Name = function;
             builder.Strparameter = arg;
-            builder.Responsetype = responsetype;
             return builder.Build();
         }
 
@@ -58,15 +54,14 @@ namespace Eryan.Factories
         /// </summary>
         /// <param name="function">The function name to be called</param>
         /// <param name="arguments">A List of Strings containing the parameters to pass with the functionCall object</param>
-        /// <param name="responsetype">The type of the expected response as given by he response struct in Responses</param>
         /// <returns>The serializeable functionCall object</returns>
-        public eveobjects.functionCall build(string function, List<string> arguments, string responsetype)
+        public eveobjects.functionCall build(string function, List<string> arguments)
         {
 
             this.function = new eveobjects.functionCall();
             eveobjects.functionCall.Builder builder = this.function.ToBuilder();
             builder.Name = function;
-            builder.Responsetype = responsetype;
+            
 
             for (int i = 0; i < arguments.Count; i++)
             {
---
+++ Factories/ResponseFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Eryan.Wrappers;
+using eveobjects;
+
+namespace Eryan.Factories
+{
+    class ResponseFactory
+    {
+        public Response build(byte[] response, string responsetype)
+        {
+            try
+            {
+                if (responsetype.Equals(Response.RESPONSES.INTERFACERESPONSE))
+                {
+                    return new InterfaceResponse(response);
+                }
+
+                return null;
+            }
+            catch (Google.ProtocolBuffers.UninitializedMessageException e)
+            {
+                Console.WriteLine("Uninitalized values");
+                return null;
+            }
+        }
+
+    }
+}
