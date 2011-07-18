d9058360a158ee3981fb53b9d30cf972e20b87ad
emistz <emistz@gmail.com>
2011-07-18 14:33:29 -0400

    All usermade scripts must implement Scriptable

---
+++ Script/Scriptable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eryan.Script
+{
+    /// <summary>
+    /// Scriptable interface must be implemented by all Eryan scripts
+    /// </summary>
+
+    public interface Scriptable
+    {
+
+        Boolean onStart();
+
+        Boolean onFinish();
+
+        int run();
+
+    }
+   
+}
