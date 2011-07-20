48387e2db6ab8b3f10aa83663dfd5511506711f8
emistz <emistz@gmail.com>
2011-07-20 03:03:16 -0400

    Scripts now have access to the communicator(for testing purposes

--- Script/Scriptable.cs
+++ Script/Scriptable.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Eryan.Input;
 using Eryan.InputHandler;
+using Eryan.IPC;
 
 namespace Eryan.Script
 {
@@ -16,13 +17,14 @@ namespace Eryan.Script
         public Mouse m;
         public PreciseMouse pm;
         public MenuHandler menuHandler;
+        public Communicator comm;
 
-
-        public void initializeInputs(PreciseMouse pm, Mouse m, MenuHandler mh)
+        public void initializeInputs(PreciseMouse pm, Mouse m, MenuHandler mh, Communicator comm)
         {
             this.m = m;
             this.pm = pm;
             this.menuHandler = mh;
+            this.comm = comm;
         }
 
         public abstract Boolean onStart();
