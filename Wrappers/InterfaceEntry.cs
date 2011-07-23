3346ec943cd024822120f143dd4c8ae3f2b9bd85
emistz <emistz@gmail.com>
2011-07-23 13:41:05 -0400

    Every object with interface data will inherit from InterfaceEtnry

---
+++ Wrappers/InterfaceEntry.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eryan.Wrappers
+{
+    /// <summary>
+    /// Base class for all interface objects
+    /// </summary>
+    public class InterfaceEntry
+    {
+        protected int x, y, width, height;
+
+        /// <summary>
+        /// Get the X position of this item in the Eve client
+        /// </summary>
+        public int X
+        {
+            get
+            {
+                return x;
+            }
+        }
+
+        /// <summary>
+        /// Get the Y position of this item in the Eve client
+        /// </summary>
+        public int Y
+        {
+            get
+            {
+                return y;
+            }
+        }
+
+
+        /// <summary>
+        /// Get the width of this item in the Eve client
+        /// </summary>
+        public int Width
+        {
+            get
+            {
+                return width;
+            }
+        }
+        
+        /// <summary>
+        /// Get the Height of this item in the Eve Client
+        /// </summary>
+        public int Height
+        {
+            get
+            {
+                return height;
+            }
+        }
+    }
+
+    
+
+}
---
+++ Wrappers/Item.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eryan.Wrappers
+{
+    /// <summary>
+    /// Wrapper for Eve Items
+    /// </summary>
+    public class Item : InterfaceEntry
+    {
+        int quantity;
+        string volume;
+        string name;
+        string meta;
+
+        public Item(int quantity, string volume, string name, string meta, int width, int height, int X, int Y)
+        {
+            this.quantity = quantity;
+            this.volume = volume;
+            this.name = name;
+            this.meta = meta;
+            this.x = X;
+            this.y = Y;
+            this.height = height;
+            this.width = width;
+        }
+
+        /// <summary>
+        /// Get the item's stack quantity
+        /// </summary>
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+        }
+
+        /// <summary>
+        /// Get the item's volume
+        /// </summary>
+        public string Volume
+        {
+            get
+            {
+                return volume;
+            }
+        }
+
+        /// <summary>
+        /// Item's name
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        /// <summary>
+        /// Item's meta level
+        /// </summary>
+        public string Meta
+        {
+            get
+            {
+                return meta;
+            }
+        }
+
+    }
+}
