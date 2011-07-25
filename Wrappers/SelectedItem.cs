4e289196ce494e7dfd248921be60301c4a6e1b37
emistz <emistz@gmail.com>
2011-07-25 01:57:21 -0400

    Wrapper to the selected item interface

---
+++ Wrappers/SelectedItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eryan.Wrappers
+{
+    /// <summary>
+    /// Wrapper for the Selected item box
+    /// </summary>
+    public class SelectedItem
+    {
+        string name;
+        int distance;
+
+        public SelectedItem(string name, int distance)
+        {
+            this.name = name;
+            this.distance = distance;
+        }
+
+        /// <summary>
+        /// The name of the selected item
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        /// <summary>
+        /// The distance to the selected item in meters
+        /// </summary>
+        public int Distance
+        {
+            get
+            {
+                return distance;
+            }
+        }
+    }
+}
