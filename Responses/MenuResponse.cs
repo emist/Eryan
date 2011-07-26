0e33160a549a39dc7fcb020d5500a6efedebd46b
emistz <emistz@gmail.com>
2011-07-26 04:07:02 -0400

    New responses and wrapper to handle menu group queries

---
+++ Responses/MenuResponse.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using eveobjects;
+using Eryan.Wrappers;
+
+namespace Eryan.Responses
+{
+    /// <summary>
+    /// Wraps an overview Response from the client
+    /// </summary>
+    public class MenuResponse : Response
+    {
+        List<MenuEntry> data;
+        overview OverViewObject;
+
+
+        /// <summary>
+        /// Concrete response for Interface replies
+        /// </summary>
+        /// <param name="input">The byte representation of the reply</param>
+        public MenuResponse(byte[] input)
+        {
+            OverViewObject = overview.CreateBuilder().MergeFrom(input).Build();
+            data = new List<MenuEntry>();
+            foreach (label lab in OverViewObject.OverviewEntryList)
+            {
+                Console.WriteLine(lab.Text);
+                Console.WriteLine(lab.TopLeftX);
+                Console.WriteLine(lab.TopLeftY);
+                  
+                data.Add(new MenuEntry(lab.Text, lab.TopLeftY, lab.TopLeftX, lab.Height, lab.Width));
+            }
+        }
+
+
+        /// <summary>
+        /// Initializes the data elements
+        /// </summary>
+        public override void HandleResponse()
+        {
+
+        }
+
+
+        /// <summary>
+        /// List represenation of the data
+        /// </summary>
+        public override Object Data
+        {
+            get
+            {
+                return data;
+            }
+        }
+    }
+}
---
+++ Wrappers/MenuEntry.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace Eryan.Wrappers
+{
+    /// <summary>
+    /// Wraps an overview entry
+    /// </summary>
+    public class MenuEntry : InterfaceEntry
+    {
+        String text;
+
+        /// <summary>
+        /// Builds an menuentry
+        /// </summary>
+        /// <param name="text">The unparsed eve client menu entry text</param>
+        /// <param name="absoluteTop">The lowest Y coordinate of the menu entry</param>
+        /// <param name="absoluteLeft">The lowest X coordinate of the menu entry</param>
+        /// <param name="height">The height of the entry on screen</param>
+        /// <param name="width">The width of the entry on screen</param>
+        public MenuEntry(string text, int absoluteTop, int absoluteLeft, int height, int width)
+        {
+            this.text = text;
+            this.y = absoluteTop;
+            this.x = absoluteLeft;
+            this.height = height;
+            this.width = width;
+        }
+
+
+        /// <summary>
+        /// Get the text of this menu entry
+        /// </summary>
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+        }
+
+    }
+}
