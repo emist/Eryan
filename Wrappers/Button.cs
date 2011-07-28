e57404f82c39bb0c5447c49120ccc234c61737e2
emistz <emistz@gmail.com>
2011-07-27 21:43:45 -0400

    Wrapper for button objects

---
+++ Wrappers/Button.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Eryan.Wrappers;
+
+
+namespace Eryan.Wrappers
+{
+    public class Button : InterfaceEntry
+    {
+        string name;
+
+        public Button(string name, int x, int y, int height, int width)
+        {
+            this.x = x;
+            this.y = y;
+            this.height = height;
+            this.width = width;
+            this.name = name;
+        }
+    }
+}
