39129c630db4641d69d9c1a079be68d209cbb268
emistz <emistz@gmail.com>
2011-08-13 23:53:13 -0400

    Config handler option

---
+++ ConfigHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Eryan
+{
+    public class ConfigHandler
+    {
+
+        public List<string> readconfig()
+        {
+            string path = System.Windows.Forms.Application.ExecutablePath;
+            path = path.Substring(0, path.Length - 9);
+            StreamReader reader = new StreamReader(path + "\\config.txt");
+            string item;
+            List<string> items = new List<string>();
+            while ((item = reader.ReadLine()) != null)
+            {
+                items.Add(item);
+            }
+
+            return items;
+        }
+    }
+}
