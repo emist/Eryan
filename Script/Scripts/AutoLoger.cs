0e9fca9c87955b40cd4a9995ab5f8721e8fc1fbf
Igor Hernandez <emistz@gmail.com>
2011-08-25 17:18:43 -0400

    Auto login background script

---
+++ Script/Scripts/AutoLoger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Drawing;
+
+using Eryan.Script;
+using Eryan.Wrappers;
+
+namespace Eryan.Script.Scripts
+{
+    /// <summary>
+    /// Logs into the Eve account
+    /// </summary>
+    public class AutoLoger : Scriptable
+    {
+
+        public override Boolean onStart()
+        {
+            return true;
+        }
+
+        public override Boolean onFinish()
+        {
+            return true;
+        }
+
+        public override int run()
+        {
+            if (ESession.isLoading())
+                return 6000;
+            
+            if (ESession.atLogin())
+            {
+                ESession.login("user", "pass");
+                return 3000;
+            }
+
+            if (ESession.atCharSel())
+                ESession.click(ESession.getEnterButton());
+           
+
+            return 200;
+        }
+    }
+}
