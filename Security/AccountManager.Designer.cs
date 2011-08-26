0e660996c887e6d38b1f633e0434cc31b6b34b8f
Igor Hernandez <emistz@gmail.com>
2011-08-25 21:34:24 -0400

    Account manager

---
+++ Security/AccountManager.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Eryan.Security
+{
+    partial class AccountManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameLbl = new System.Windows.Forms.Label();
+            this.passwordLbl = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.okBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usernameLbl
+            // 
+            this.usernameLbl.AutoSize = true;
+            this.usernameLbl.Location = new System.Drawing.Point(15, 44);
+            this.usernameLbl.Name = "usernameLbl";
+            this.usernameLbl.Size = new System.Drawing.Size(55, 13);
+            this.usernameLbl.TabIndex = 0;
+            this.usernameLbl.Text = "Username";
+            // 
+            // passwordLbl
+            // 
+            this.passwordLbl.AutoSize = true;
+            this.passwordLbl.Location = new System.Drawing.Point(17, 89);
+            this.passwordLbl.Name = "passwordLbl";
+            this.passwordLbl.Size = new System.Drawing.Size(53, 13);
+            this.passwordLbl.TabIndex = 1;
+            this.passwordLbl.Text = "Password";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(112, 42);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(128, 20);
+            this.username.TabIndex = 2;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(113, 88);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(126, 20);
+            this.password.TabIndex = 3;
+            // 
+            // okBtn
+            // 
+            this.okBtn.Location = new System.Drawing.Point(113, 138);
+            this.okBtn.Name = "okBtn";
+            this.okBtn.Size = new System.Drawing.Size(75, 23);
+            this.okBtn.TabIndex = 4;
+            this.okBtn.Text = "OK";
+            this.okBtn.UseVisualStyleBackColor = true;
+            this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
+            // 
+            // AccountManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 173);
+            this.Controls.Add(this.okBtn);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.passwordLbl);
+            this.Controls.Add(this.usernameLbl);
+            this.Name = "AccountManager";
+            this.Text = "AccountManager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label usernameLbl;
+        private System.Windows.Forms.Label passwordLbl;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Button okBtn;
+    }
+}
---
+++ Security/AccountManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Eryan.Security
+{
+    public partial class AccountManager : Form
+    {
+        
+        public AccountManager()
+        {
+            InitializeComponent();
+        }
+
+        public string Username
+        {
+            get
+            {
+                return username.Text;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password.Text;
+            }
+        }
+
+        private void okBtn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
