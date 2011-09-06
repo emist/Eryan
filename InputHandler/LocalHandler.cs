511d5a4665a967c572da759950df73a7a165987a
Igor Hernandez <emistz@gmail.com>
2011-09-05 23:09:22 -0400

    Read and Write to local

--- InputHandler/LocalHandler.cs
+++ InputHandler/LocalHandler.cs
@@ -24,6 +24,7 @@ namespace Eryan.InputHandler
         PreciseMouse pm;
         Random ran;
         MenuHandler mh;
+        KeyBoard kb;
 
         public LocalHandler(WindowHandler wh)
         {
@@ -32,8 +33,13 @@ namespace Eryan.InputHandler
             pm = wh.PMOUSE;
             ran = new Random();
             mh = wh.MENU;
+            kb = wh.KEYBOARD;
         }
 
+        /// <summary>
+        /// Get the scrollbar to the userlist
+        /// </summary>
+        /// <returns>The scrollbar or null on failure</returns>
         public override InterfaceEntry userlistScrollBar()
         {
             InterfaceResponse iresp = (InterfaceResponse)com.sendCall(FunctionCallFactory.CALLS.GETLOCALCHATSCROLLBAR, Response.RESPONSES.INTERFACERESPONSE);
@@ -46,6 +52,10 @@ namespace Eryan.InputHandler
 
         }
 
+        /// <summary>
+        /// Scroll down the userlist
+        /// </summary>
+        /// <returns>True on success, false on failure</returns>
         public override bool userlistScrollDown()
         {
             InterfaceResponse iresp = (InterfaceResponse)com.sendCall(FunctionCallFactory.CALLS.GETLOCALCHATSCROLLBAR, Response.RESPONSES.INTERFACERESPONSE);
@@ -62,6 +72,10 @@ namespace Eryan.InputHandler
             return true;
         }
 
+        /// <summary>
+        /// Scroll the userlist all the way down
+        /// </summary>
+        /// <returns>True on success, false on failure</returns>
         public bool userlistScrollToBottom()
         {
             InterfaceEntry scrollbar = userlistScrollBar();
@@ -78,6 +92,47 @@ namespace Eryan.InputHandler
             return true;
         }
 
+        /// <summary>
+        /// Write the given string to local chat
+        /// </summary>
+        /// <param name="text">The text to write</param>
+        /// <returns>True on success, false otherwise</returns>
+        public bool writeToLocal(string text)
+        {
+            InterfaceResponse iresp = (InterfaceResponse)com.sendCall(FunctionCallFactory.CALLS.GETLOCALWRITINGAREA, Response.RESPONSES.INTERFACERESPONSE);
+            if (iresp == null)
+            {
+                return false;
+            }
+
+            m.move(new Point(ran.Next(iresp.X + 5, iresp.X + iresp.Width - 5), ran.Next(iresp.Y + 5, iresp.Y + iresp.Height - 5)));
+            Thread.Sleep(ran.Next(200, 400));
+            m.click(true);
+            Thread.Sleep(ran.Next(200, 300));
+            kb.sendKeyPresses(text);
+            Thread.Sleep(ran.Next(300, 400));
+            kb.pressEnter();
+            pm.synchronize(m);
+            return true;
+        }
+        /// <summary>
+        /// Get the last 15 entries in local chat for the given solarsystemid
+        /// </summary>
+        /// <param name="ssid">The solar system id to retrieve the local chat from</param>
+        /// <returns>The list of entries on success, null on failure</returns>
+        public List<String> readLocal(int ssid)
+        {
+            StringGroupResponse sresp = (StringGroupResponse)com.sendCall(FunctionCallFactory.CALLS.GETLOCALCHATTEXT, ssid.ToString(), Response.RESPONSES.STRINGGROUPRESPONSE);
+            if (sresp == null)
+                return null;
+
+            return (List<String>)sresp.Data;
+        }
+
+        /// <summary>
+        /// Scroll the userlist to the top
+        /// </summary>
+        /// <returns>True on success, false on failure</returns>
         public bool userlistScrollToTop()
         {
             InterfaceEntry scrollbar = userlistScrollBar();
@@ -95,6 +150,10 @@ namespace Eryan.InputHandler
             return true;
         }
 
+        /// <summary>
+        /// Scroll up the userlist
+        /// </summary>
+        /// <returns>True on success, false on failure</returns>
         public override bool userlistScrollUp()
         {
             InterfaceResponse iresp = (InterfaceResponse)com.sendCall(FunctionCallFactory.CALLS.GETLOCALCHATSCROLLBAR, Response.RESPONSES.INTERFACERESPONSE);
@@ -110,6 +169,11 @@ namespace Eryan.InputHandler
             return true;   
         }
 
+        /// <summary>
+        /// Get a character entry on the userlist
+        /// </summary>
+        /// <param name="charname">The name of the char to get</param>
+        /// <returns>The entry for the char on success, null on failure</returns>
         public override InterfaceEntry userlistGetEntry(string charname)
         {
             InterfaceResponse iresp = (InterfaceResponse)com.sendCall(FunctionCallFactory.CALLS.FINDPLAYERINLOCAL, charname, Response.RESPONSES.INTERFACERESPONSE);
@@ -120,6 +184,10 @@ namespace Eryan.InputHandler
             return new InterfaceEntry(iresp.X, iresp.Y, iresp.Width, iresp.Height);
         }
 
+        /// <summary>
+        /// Get the bottom position of the userlist
+        /// </summary>
+        /// <returns>The bottom on success, -1 on failure</returns>
         public override int userlistBottom()
         {
             StringResponse sresp = (StringResponse)com.sendCall(FunctionCallFactory.CALLS.GETLOCALCHATBOTTOM, Response.RESPONSES.STRINGRESPONSE);
@@ -131,6 +199,10 @@ namespace Eryan.InputHandler
             return Convert.ToInt32((string)sresp.Data);
         }
 
+        /// <summary>
+        /// Get the top of the userlist
+        /// </summary>
+        /// <returns>The top on success, -1 on failure</returns>
         public override int userlistTop()
         {
             StringResponse sresp = (StringResponse)com.sendCall(FunctionCallFactory.CALLS.GETLOCALCHATTOP, Response.RESPONSES.STRINGRESPONSE);
@@ -142,6 +214,11 @@ namespace Eryan.InputHandler
             return Convert.ToInt32((string)sresp.Data);
         }
 
+        /// <summary>
+        /// Open a menu on the given character entry from the userlist
+        /// </summary>
+        /// <param name="charName">The character name to open the menu on</param>
+        /// <returns>True on success, false otherwise</returns>
         public override bool openMenu(string charName)
         {
             int passes = 0;
