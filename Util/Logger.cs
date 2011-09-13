14fd62bb6826a4f8129f555fed92f93f03df35ba
Igor Hernandez <emistz@gmail.com>
2011-09-12 23:16:02 -0400

    Logging engine

---
+++ Util/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eryan.Util
+{
+    public class Logger
+    {
+        string scriptname;
+        UI.LogViewer logViewer;
+
+        public Logger(UI.WindowHandler wh, string ScriptName)
+        {
+            scriptname = ScriptName;
+            logViewer = wh.LOGVIEWER;
+        }
+
+
+
+        public void log(string message)
+        {
+            logViewer.LogBox.Text += scriptname + ": " + message + "\n";
+        }
+
+        public string ScriptName
+        {
+            get
+            {
+                return scriptname;
+            }
+
+            set
+            {
+                scriptname = value;
+            }
+        }
+    }
+}
